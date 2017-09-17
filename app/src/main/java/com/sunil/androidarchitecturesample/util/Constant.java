83668b62adbf96fd3cefc15fc2b8822199361d4c
sunil <sunil.gupta676@gmail.com>
2017-09-17 14:37:07 +0530

    added detail

--- app/src/main/java/com/sunil/androidarchitecturesample/util/Constant.java
+++ app/src/main/java/com/sunil/androidarchitecturesample/util/Constant.java
@@ -7,6 +7,6 @@ package com.sunil.androidarchitecturesample.util;
 public class Constant {
     public static final String ENDPOINT = "https://api.themoviedb.org/3/";
     public static final String IMAGE_ENDPOINT_PREFIX = "https://image.tmdb.org/t/p/w500/";
-    public static final String API_KEY = "";
+    public static final String API_KEY = "c9a058c168b2ee5563bada0f30c7b0e8";
     public static final int TIMEOUT_IN_SEC = 15;
 }
